6578624f258c1540a3c35318aae1122207fbcd17
VisheshRampersad <visheshrampersad2005@gmail.com>
2025-11-25 21:37:36 +0000

    Fixed admin.py and shift.py for testing

--- App/controllers/admin.py
+++ App/controllers/admin.py
@@ -1,7 +1,8 @@
-from App.models import Shift
-from App.database import db
-from datetime import datetime
-from App.controllers.user import get_user
+#from App.models import Shift
+#from App.database import db
+#from datetime import datetime
+#from App.controllers.user import get_user
+#all of the above were duplicated imports
 
 from App.models import Shift, Schedule
 from App.database import db
@@ -23,6 +24,20 @@ def create_schedule(admin_id, scheduleName): #Not sure why this was missing
     db.session.commit()
 
     return new_schedule
+
+def create_unassigned_shift(start_time, end_time):
+    new_shift = Shift(
+        staff_id=None,
+        schedule_id=None,
+        start_time=start_time,
+        end_time=end_time
+    )
+
+    db.session.add(new_shift)
+    db.session.commit()
+
+    return new_shift
+
 
 def schedule_shift(admin_id, staff_id, schedule_id, start_time, end_time):
     admin = get_user(admin_id)
